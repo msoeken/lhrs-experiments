0cde0d107c2eabf512fb470fcc2860fcf997bce3
Mathias Soeken <mathias.soeken@epfl.ch>
2018-02-16 21:04:31 -0800

    HDBS.

--- lib/synthesis.py
+++ lib/synthesis.py
@@ -1,3 +1,5 @@
+from timeit import default_timer as timer
+
 import revkit
 
 def lhrs(filename, configuration):
@@ -28,5 +30,22 @@ def cbs(filename, configuration):
 
     return synthesis, circuit
 
+def hdbs(filename, configuration):
+    revkit.read_aiger(filename = filename)
+    num_outputs = revkit.ps(aig = True, silent = True)["outputs"]
+
+    # also incorporate conversion to BDD to the runtime
+    start = timer()
+    revkit.convert(aig_to_bdd = True)
+    synthesis = revkit.hdbs(**configuration).dict()
+    end = timer()
+    synthesis["runtime"] = end - start
+
+    circuit = revkit.ps(circuit = True, silent = True).dict()
+    circuit["qubits"] += num_outputs
+    circuit["tcount"] *= 2
+
+    return synthesis, circuit
+
 def get_default_algorithms():
-    return {'lhrs': lhrs, 'dxs': dxs, 'cbs': cbs}
+    return {'lhrs': lhrs, 'dxs': dxs, 'cbs': cbs, 'hdbs': hdbs}
