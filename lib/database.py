c00318e949b6374117aba7f54abce3d8d933e2fc
Mathias Soeken <mathias.soeken@epfl.ch>
2018-02-21 18:22:07 +0100

    Database and utility functions.

--- lib/database.py
+++ lib/database.py
@@ -1,5 +1,6 @@
+import functools
 from tinydb import TinyDB, Query
-
+import numpy as np
 
 class Database:
     def __init__(self, filename="lhrs.json"):
@@ -18,6 +19,11 @@ class Database:
         assert(len(entries) == 1)
         return entries[0]
 
+    def get_entries(self, *benchmarks):
+        q = Query()
+        query = functools.reduce(lambda a, b: a | b, [q.benchmark == name for name in benchmarks])
+        return self.db.search(query)
+
     def insert_entry(self, benchmark, method, configuration, synthesis, circuit):
         self.db.insert({'benchmark': benchmark, 'method': method, 'configuration': configuration,
                         'synthesis': synthesis, 'circuit': circuit})
@@ -31,3 +37,27 @@ class Database:
             return int(entry["circuit"]["qubits"]), int(entry["circuit"]["tcount"]), entry["synthesis"]["runtime"]
         else:
             return [self.get_main_statistics(b, method, configuration) for b in benchmark]
+
+    def get_class_counters(self, configuration):
+        def counter_as_array(counter):
+            return [np.array(l) for l in counter]
+
+        q = Query()
+        entries = self.db.search(q.configuration == configuration)
+        return [sum(l) for l in zip(*[counter_as_array(e["synthesis"]["class_counter"]) for e in entries])]
+
+        # sums = None
+
+        # q = Query()
+        # entries = self.db.search(q.configuration == configuration)
+        # for e in entries:
+        #     counters = [np.array(l) for l in e["synthesis"]["class_counter"]]
+
+        #     if sums is None:
+        #         sums = counters
+        #     else:
+        #         sums = [sum(l) for l in zip(sums, counters)]
+
+        # return sums
+
+
---
+++ lib/utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def thousand_sep(number):
+    if isinstance(number, int) or isinstance(number, np.int64):
+        return '{:,}'.format(number)
+    elif isinstance(number, float):
+        return '{:,.2f}'.format(number)
+    else:
+        return number
+
+def make_pareto(entries):
+    # sort entries
+    entries = sorted(entries, key = lambda e: (e["circuit"]["tcount"], e["circuit"]["qubits"]))
+    
+    if len(entries) == 0:
+        return entries
+    
+    min_qubits = entries[0]["circuit"]["qubits"]
+    entries[0]["pareto"] = True
+
+    for e in entries[1:]:
+        qubits = e["circuit"]["qubits"]
+        if qubits < min_qubits:
+            min_qubits = qubits
+            e["pareto"] = True
+        else:
+            e["pareto"] = False
+            
+    return entries
